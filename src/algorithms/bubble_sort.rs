f9809ae55d6391dcb3ca0b2aac40e70ec5f5983a
hens <121116111@qq.com>
2024-09-18 16:12:52 +0800

    feat: bubble sort

--- src/algorithms/bubble_sort.rs
+++ src/algorithms/bubble_sort.rs
@@ -1,4 +1,15 @@
-fn bubble_sort(arr: &mut [i32]) {}
+fn bubble_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    let size = len - 1;
+
+    for i in 0..size {
+        for j in 0..(size - i) {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,7 +17,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut vec = vec![67, 22, 3];
-        bubble_sort(&mut vec)
+        let mut vec = vec![67, 22, 3, 2, 1];
+        bubble_sort(&mut vec);
+        assert_eq!(vec, vec![1, 2, 3, 22, 67]);
     }
 }
